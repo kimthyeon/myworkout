cc8c6f02abb936cfdf1246c29d46df1973a1bfae
kim <sugoxalr@gmail.com>
2022-09-02 12:49:47 +0900

    공지사항 테이블, 데이터

---
+++ sql/create-table.sql
@@ -0,0 +1,6 @@
+# 공지사항
+CREATE TABLE `myworkout`.`notice` (
+                                      `id` INT NOT NULL AUTO_INCREMENT,
+                                      `nt_title` VARCHAR(100) NOT NULL,
+                                      `nt_content` VARCHAR(300) NOT NULL,
+                                      PRIMARY KEY (`id`));
